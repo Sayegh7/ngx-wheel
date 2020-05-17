63744ec81e6137e6c45b2609e05812233bc53b00
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2020-05-17 12:45:09 +0200

    Updates

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-export { NgxWheelModule } from './ngx-wheel.module';
-export { NgxWheelComponent } from './ngx-wheel.component';
--- ngx-wheel.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { NgxWheelComponent } from './ngx-wheel.component';
-export declare function NgxWheelComponentFac(): NgxWheelComponent;
-export declare class NgxWheelModule {
-}
